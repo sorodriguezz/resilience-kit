bab382985dc730b3bd01f6289cf71bc736248580
sorodriguezz <s.rodriguezzapata01@gmail.com>
2025-01-29 18:28:37 -0300

    add Service for JS

--- src/patterns/index.ts
+++ src/patterns/index.ts
@@ -3,3 +3,8 @@ export * from './retry/retry.decorator';
 export * from './timeout/timeout.decorator';
 export * from './fallback/fallback.decorator';
 export * from './resilience-chain.decorator';
+
+export { RetryService } from './retry/retry.service';
+export { TimeoutService } from './timeout/timeout.service';
+export { CircuitBreakerService } from './circuit-breaker/circuit-breaker.service';
+export { FallbackService } from './fallback/fallback.service';
